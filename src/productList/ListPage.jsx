e913c315589be3099db67a59337569e41b4731a6
riturajmahapatra <riturajmahapatra@gmail.com>
2023-09-09 15:26:26 +0530

    list page

--- src/productList/ListPage.jsx
+++ src/productList/ListPage.jsx
@@ -12,7 +12,13 @@ const ListPage = () => {
       <Image
         image="https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
         customclass={'h-[40vh] w-full object-cover'}></Image>
-      <CardSection />
+      <div className="my-5 grid items-center justify-center gap-5">
+        <CardSection />
+        <CardSection />
+        <CardSection />
+        <CardSection />
+        <CardSection />
+      </div>
       <Footer />
     </div>
   )
