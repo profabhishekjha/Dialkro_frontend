7aa56538b303c85e419541f70d3a0276b2e8fd3e
riturajmahapatra <riturajmahapatra@gmail.com>
2023-09-13 16:59:37 +0530

    ac pages

--- src/repairServices/acRepair/AcRepairPage.jsx
+++ src/repairServices/acRepair/AcRepairPage.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import Nav from '../../components/Navbar/Nav'
-import { Outlet } from 'react-router-dom'
-import Image from '../../components/Body/Carousel/Image'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from '../../components/Footer/Footer'
 import CardSection from '../../productList/components/cardSection'
 import { Toaster } from 'react-hot-toast'
+import BannerCarousel from '../../components/Body/Carousel/BannerCarousel'
 const AcRepairPage = () => {
+  const ac = 'ac'
+
+  const images = [
+    `Hero_pg1/Repairs/carCarousel/car (1).webp`,
+    `Hero_pg1/Repairs/${ac}Carousel/${ac} (2).webp`,
+    `Hero_pg1/Repairs/${ac}Carousel/${ac} (3).webp`,
+    `Hero_pg1/Repairs/${ac}Carousel/${ac} (4).webp`,
+    `Hero_pg1/Repairs/${ac}Carousel/${ac} (5).webp`
+  ]
+
+  const navigate = useNavigate()
   return (
     <div>
       <Outlet />
       <Toaster />
       <Nav />
-      <Image
-        image="/Hero_pg1/Popular/banner.webp"
-        customclass={'h-[40vh] w-full object-cover'}></Image>
+
+      <BannerCarousel images={images} />
       <div className=" my-5 grid  items-center justify-center gap-5">
-        <CardSection />
-        <CardSection />
-        <CardSection />
-        <CardSection />
-        <CardSection />
+        <CardSection onClick={() => navigate('/acRepairPage/Product_Detail')} />
+        <CardSection onClick={() => navigate('/acRepairPage/Product_Detail')} />
+        <CardSection onClick={() => navigate('/acRepairPage/Product_Detail')} />
+        <CardSection onClick={() => navigate('/acRepairPage/Product_Detail')} />
+        <CardSection onClick={() => navigate('/acRepairPage/Product_Detail')} />
+        <CardSection onClick={() => navigate('/acRepairPage/Product_Detail')} />
       </div>
       <Footer />
     </div>
