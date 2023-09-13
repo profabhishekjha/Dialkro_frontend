b81c3dc15718dfdc5818b45a7f89fbdb94af8842
riturajmahapatra <riturajmahapatra@gmail.com>
2023-09-13 15:50:34 +0530

    wedding-pages-redirection

--- src/weddingAndBeauty/bridalMakeup/BridalMakeupPage.jsx
+++ src/weddingAndBeauty/bridalMakeup/BridalMakeupPage.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import Nav from '../../components/Navbar/Nav'
-import { Outlet } from 'react-router-dom'
-import Image from '../../components/Body/Carousel/Image'
+import { Outlet, useNavigate } from 'react-router-dom'
+import BannerCarousel from '../../components/Body/Carousel/BannerCarousel'
 import Footer from '../../components/Footer/Footer'
 import CardSection from '../../productList/components/cardSection'
 import { Toaster } from 'react-hot-toast'
 const BridalMakeupPage = () => {
+  const bridal = 'bridal'
+
+  const images = [
+    `Hero_pg1/Wedding&Beauty/${bridal}Carousel/${bridal}5.webp`,
+    `Hero_pg1/Wedding&Beauty/${bridal}Carousel/${bridal}2.webp`,
+    `Hero_pg1/Wedding&Beauty/${bridal}Carousel/${bridal}7.webp`,
+    `Hero_pg1/Wedding&Beauty/${bridal}Carousel/${bridal}6.webp`,
+    `Hero_pg1/Wedding&Beauty/${bridal}Carousel/${bridal}8.webp`
+  ]
+
+  const navigate = useNavigate()
+
   return (
     <div>
       <Outlet />
       <Toaster />
       <Nav />
-      <Image
-        image="/Hero_pg1/Popular/banner.webp"
-        customclass={'h-[40vh] w-full object-cover'}></Image>
+      <BannerCarousel images={images} />
       <div className=" my-5 grid  items-center justify-center gap-5">
-        <CardSection />
-        <CardSection />
-        <CardSection />
-        <CardSection />
-        <CardSection />
+        <CardSection onClick={() => navigate('/bridalMakeup/Product_Detail')} />
+        <CardSection onClick={() => navigate('/bridalMakeup/Product_Detail')} />
+        <CardSection onClick={() => navigate('/bridalMakeup/Product_Detail')} />
+        <CardSection onClick={() => navigate('/bridalMakeup/Product_Detail')} />
+        <CardSection onClick={() => navigate('/bridalMakeup/Product_Detail')} />
+        <CardSection onClick={() => navigate('/bridalMakeup/Product_Detail')} />
       </div>
       <Footer />
     </div>
