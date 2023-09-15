0258b1b72f25843d32a3c79b362f8cdb0212beca
profabhishekjha <jhaabhishek910@gmail.com>
2023-09-15 15:00:14 +0530

    structure

--- src/components/ui/AlignmentSubCard.jsx
+++ src/components/ui/AlignmentSubCard.jsx
@@ -6,7 +6,7 @@ const AlignmentSubCards = ({ images, prompt, onClick }) => {
     <div className="z-10 flex">
       <div className="relative p-3">
         <div className=" relative mx-auto h-24 w-32 overflow-hidden rounded-lg bg-white shadow-lg">
-          <a href="" onClick={onClick}>
+          <a onClick={onClick} className="cursor-pointer">
             <img className="h-24 w-full object-cover" src={images} alt="Card Image" />
             <figcaption className="absolute inset-0 z-10 flex items-center justify-center bg-white bg-opacity-100 px-4 text-black opacity-0 transition-all hover:text-xl hover:font-bold hover:opacity-60">
               <p className="text-center uppercase ">{prompt}</p>
