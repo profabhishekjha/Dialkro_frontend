5cea15ccd0c6c7a524ac145d381f26fe6bcaf2e8
profabhishekjha <jhaabhishek910@gmail.com>
2023-09-18 12:57:13 +0530

    ads and detail section

--- src/components/ui/AlignmentCards.jsx
+++ src/components/ui/AlignmentCards.jsx
@@ -5,9 +5,9 @@ const AlignmentCards = ({ images, prompt, onClick }) => {
   return (
     <div className="z-10 flex items-center justify-center">
       <div className="relative p-2">
-        <div className=" relative mx-auto h-32 w-48 overflow-hidden rounded-lg bg-white shadow-lg">
+        <div className=" relative mx-auto h-24 w-32 overflow-hidden rounded-lg bg-white shadow-lg">
           <a onClick={onClick} className="cursor-pointer">
-            <img className="h-32 w-full object-cover" src={images} alt="Card Image" />
+            <img className="h-24 w-full object-cover" src={images} alt="Card Image" />
             <figcaption className="absolute inset-0 z-10 flex items-center justify-center bg-white bg-opacity-100 px-4 text-black opacity-0 transition-all hover:text-xl hover:font-bold hover:opacity-60">
               <p className="text-center uppercase ">{prompt}</p>
             </figcaption>
