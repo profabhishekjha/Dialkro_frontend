82c716d528242bbe2a918da09ae378f9ddccac5c
profabhishekjha <jhaabhishek910@gmail.com>
2023-09-18 17:53:33 +0530

    faker

--- src/ProductDetails/ProductShopDetails.jsx
+++ src/ProductDetails/ProductShopDetails.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
+import { faker } from '@faker-js/faker'
 
 const ProductShopDetails = () => {
   return (
     <div>
       <div className="mt-10 h-[430px] w-[380px] rounded-lg border">
-        <div className="flex items-center justify-center">
+        <div className="redborder flex items-center justify-center p-3">
           <div>
             <h2 className="mb-6 mt-5 w-80 text-2xl font-semibold">Address</h2>
-            <p className="text-lg">Bijwasan, Delhi - 110061</p>
+            <p className="overflow-hidden text-lg">{`${faker.location.buildingNumber()}, ${faker.location.city()}, ${faker.location.city()} - ${faker.location.zipCode(
+              '######'
+            )} `}</p>
             <hr className="mb-3 mt-3" />
-            <p className="flex gap-3 text-lg">
-              <img src="/productDetail/printer.svg" alt="" className="w-5" /> GSTIN :
-              07AAUCA7674F1Z4
+            <p className="flex gap-5 text-lg uppercase">
+              <img src="/productDetail/printer.svg" alt="" className="w-5" />
+              {`GSTIN : ${faker.random.alphaNumeric(12)}`}
             </p>
             <hr className="mb-3 mt-3" />
-            <a className="flex gap-3 text-lg">
+            <a href="#" className="flex gap-5 text-lg">
               <img src="/productDetail/mobile.svg" alt="" className="w-4" /> Get info via SMS/Email
             </a>
             <hr className="mb-3 mt-3" />
-            <a className="flex gap-3 text-lg">
+            <a href="#" className="flex gap-5 text-lg">
               <img src="/productDetail/share.svg" alt="" className="w-6" />
               Share this
             </a>
             <hr className="mb-3 mt-3" />
-            <a className="flex gap-3 text-lg">
+            <a href="#" className="flex gap-5 text-lg">
               <img src="/productDetail/star.svg" alt="" className="w-6" /> Tap to Rate
             </a>
             <hr className="mb-3 mt-3" />
-            <a className="flex gap-3 text-lg">
+            <a href={`https://www.${faker.internet.domainName()}/`} className="flex gap-5 text-lg">
               <img src="/productDetail/web.svg" alt="" className="w-5" />
-              Visit our website
+              {`${faker.internet.domainName()}`}
             </a>
             <hr className="mb-3 mt-3" />
           </div>
