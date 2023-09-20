74e65906e25d0b027f2a4bd057be95a83109d11f
riturajmahapatra <riturajmahapatra@gmail.com>
2023-09-20 12:33:56 +0530

    nav-footer added

--- src/components/Navbar/addBusiness/BusinessListingForm.jsx
+++ src/components/Navbar/addBusiness/BusinessListingForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import Nav from '../Nav'
+import Footer from '../../Footer/Footer'
 
 const BusinessListingForm = () => {
   const navigate = useNavigate()
@@ -30,137 +32,144 @@ const BusinessListingForm = () => {
   }
 
   return (
-    <div className="flex items-center justify-center" onSubmit={() => navigate('/businessform')}>
-      <form className=" container mt-48 w-5/12 rounded-lg border p-10" onSubmit={handleSubmit}>
-        <div className="mb-5 text-center">
-          <h1 className="text-xl font-light">
-            Get your business listed for{' '}
-            <span className="bg-yellow-400 px-1 font-semibold">FREE</span> on {`India's`} premier
-            local search engine!
-          </h1>
-        </div>
-        <div className="mb-6">
-          <label htmlFor="companyName" className="mb-2 block text-sm font-medium text-gray-900">
-            Company Name <span className="text-red-700">*</span>
-          </label>
-          <input
-            type="text"
-            id="companyName"
-            name="companyName"
-            value={formData.companyName}
-            onChange={handleChange}
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400"
-            placeholder="DialKro"
-            required
-          />
-        </div>
-        {/* ... Existing JSX code ... */}
-        <div className="grid grid-cols-3 gap-10">
-          <div className="col-span-1 mb-6">
-            <label htmlFor="title" className="mb-2 block text-sm font-medium text-gray-900">
-              Title
-            </label>
-            <select
-              id="title"
-              name="title"
-              value={formData.title}
-              onChange={handleChange}
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400">
-              <option value="">-Select-</option>
-              <option value="Mr">Mr</option>
-              <option value="Mrs">Mrs</option>
-              <option value="Others">Ms</option>
-              <option value="Others">Dr.</option>
-            </select>
+    <>
+      <Nav />
+
+      <div className="flex items-center justify-center" onSubmit={() => navigate('/businessform')}>
+        <form className=" container mt-48 w-5/12 rounded-lg border p-10" onSubmit={handleSubmit}>
+          <div className="mb-5 text-center">
+            <h1 className="text-xl font-light">
+              Get your business listed for{' '}
+              <span className="bg-yellow-400 px-1 font-semibold">FREE</span> on {`India's`} premier
+              local search engine!
+            </h1>
           </div>
           <div className="mb-6">
-            <label htmlFor="fullName" className="mb-2 block text-sm font-medium text-gray-900">
-              Full Name <span className="text-red-700">*</span>
+            <label htmlFor="companyName" className="mb-2 block text-sm font-medium text-gray-900">
+              Company Name <span className="text-red-700">*</span>
             </label>
             <input
               type="text"
-              id="fullName"
-              name="fullName"
-              value={formData.fullName}
+              id="companyName"
+              name="companyName"
+              value={formData.companyName}
               onChange={handleChange}
               className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400"
-              placeholder="John Doe"
+              placeholder="DialKro"
               required
             />
           </div>
+          {/* ... Existing JSX code ... */}
+          <div className="grid grid-cols-3 gap-10">
+            <div className="col-span-1 mb-6">
+              <label htmlFor="title" className="mb-2 block text-sm font-medium text-gray-900">
+                Title
+              </label>
+              <select
+                id="title"
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400">
+                <option value="">-Select-</option>
+                <option value="Mr">Mr</option>
+                <option value="Mrs">Mrs</option>
+                <option value="Others">Ms</option>
+                <option value="Others">Dr.</option>
+              </select>
+            </div>
+            <div className="mb-6">
+              <label htmlFor="fullName" className="mb-2 block text-sm font-medium text-gray-900">
+                Full Name <span className="text-red-700">*</span>
+              </label>
+              <input
+                type="text"
+                id="fullName"
+                name="fullName"
+                value={formData.fullName}
+                onChange={handleChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400"
+                placeholder="John Doe"
+                required
+              />
+            </div>
 
-          <div className="mb-6">
-            <label
-              htmlFor="mobileNumber"
-              className="mb-2 block font-medium text-gray-900"
-              style={{ fontSize: '14px' }}>
-              Mobile Number <span className="text-red-700">*</span>
-            </label>
-            <input
-              type="text"
-              id="mobileNumber"
-              name="mobileNumber"
-              value={formData.mobileNumber}
-              onChange={handleChange}
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400"
-              placeholder="(+91) 000-000-0000"
-            />
+            <div className="mb-6">
+              <label
+                htmlFor="mobileNumber"
+                className="mb-2 block font-medium text-gray-900"
+                style={{ fontSize: '14px' }}>
+                Mobile Number <span className="text-red-700">*</span>
+              </label>
+              <input
+                type="text"
+                id="mobileNumber"
+                name="mobileNumber"
+                value={formData.mobileNumber}
+                onChange={handleChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400"
+                placeholder="(+91) 000-000-0000"
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="grid grid-cols-2 gap-10">
-          <div className="mb-6">
-            <label htmlFor="companyEmail" className="mb-2 block text-sm font-medium text-gray-900">
-              Company Email <span className="text-red-700">*</span>
+          <div className="grid grid-cols-2 gap-10">
+            <div className="mb-6">
+              <label
+                htmlFor="companyEmail"
+                className="mb-2 block text-sm font-medium text-gray-900">
+                Company Email <span className="text-red-700">*</span>
+              </label>
+              <input
+                type="email"
+                id="companyEmail"
+                name="companyEmail"
+                value={formData.companyEmail}
+                onChange={handleChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400"
+                placeholder="Dialkro@gmail.com"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="companyURL" className="mb-2 block text-sm font-medium text-gray-900">
+                Company URL <span className="text-red-700">*</span>
+              </label>
+              <input
+                type="www"
+                id="companyURL"
+                name="companyURL"
+                value={formData.companyURL}
+                onChange={handleChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400"
+                placeholder="DialKro.com"
+              />
+            </div>
+          </div>
+          <div className="mb-6 flex items-start">
+            <div className="flex h-5 items-center">
+              <input
+                id="remember"
+                name="rememberMe"
+                type="checkbox"
+                checked={formData.rememberMe}
+                onChange={handleChange}
+                className="h-4 w-4 rounded border border-gray-300 bg-gray-50"
+              />
+            </div>
+            <label htmlFor="remember" className="ml-3 text-sm font-medium text-gray-400">
+              Remember me
             </label>
-            <input
-              type="email"
-              id="companyEmail"
-              name="companyEmail"
-              value={formData.companyEmail}
-              onChange={handleChange}
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400"
-              placeholder="Dialkro@gmail.com"
-              required
-            />
           </div>
-          <div className="mb-6">
-            <label htmlFor="companyURL" className="mb-2 block text-sm font-medium text-gray-900">
-              Company URL <span className="text-red-700">*</span>
-            </label>
-            <input
-              type="www"
-              id="companyURL"
-              name="companyURL"
-              value={formData.companyURL}
-              onChange={handleChange}
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:placeholder-gray-400"
-              placeholder="DialKro.com"
-            />
-          </div>
-        </div>
-        <div className="mb-6 flex items-start">
-          <div className="flex h-5 items-center">
-            <input
-              id="remember"
-              name="rememberMe"
-              type="checkbox"
-              checked={formData.rememberMe}
-              onChange={handleChange}
-              className="h-4 w-4 rounded border border-gray-300 bg-gray-50"
-            />
-          </div>
-          <label htmlFor="remember" className="ml-3 text-sm font-medium text-gray-400">
-            Remember me
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">
-          Submit
-        </button>
-      </form>
-    </div>
+          <button
+            type="submit"
+            className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">
+            Submit
+          </button>
+        </form>
+      </div>
+      <Footer />
+    </>
   )
 }
 
--- src/components/Navbar/addBusiness/PaymentForm.jsx
+++ src/components/Navbar/addBusiness/PaymentForm.jsx
@@ -1,172 +1,179 @@
 import React from 'react'
 import ShopTimingsForm from './ShopTimingsForm'
 import { useNavigate } from 'react-router-dom'
+import Nav from '../Nav'
+import Footer from '../../Footer/Footer'
 const PaymentForm = () => {
   const navigate = useNavigate()
   return (
-    <div className="flex items-center justify-center">
-      <form className="container mt-10 rounded-lg border p-10">
-        <h2 className="mb-4 p-3 text-center text-3xl font-semibold">Other Information</h2>
-        <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
-        <ShopTimingsForm />
-        <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
+    <>
+      <Nav />
 
-        <h2 className="mb-4 p-3 text-center text-2xl font-semibold">
-          <span className="bg-yellow-300 px-1">Select Payment Gateways</span> Accepted by you
-        </h2>
+      <div className="flex items-center justify-center">
+        <form className="container mt-10 rounded-lg border p-10">
+          <h2 className="mb-4 p-3 text-center text-3xl font-semibold">Other Information</h2>
+          <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
+          <ShopTimingsForm />
+          <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
 
-        <div className="flex items-center justify-center gap-12">
-          <div className="space-y-4">
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="googlepay"
-              />
-              <span className="text-lg ">Google Pay</span>
-            </label>
+          <h2 className="mb-4 p-3 text-center text-2xl font-semibold">
+            <span className="bg-yellow-300 px-1">Select Payment Gateways</span> Accepted by you
+          </h2>
 
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="phonepe"
-              />
-              <span className="text-lg">PhonePe</span>
-            </label>
+          <div className="flex items-center justify-center gap-12">
+            <div className="space-y-4">
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="googlepay"
+                />
+                <span className="text-lg ">Google Pay</span>
+              </label>
 
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="bhim"
-              />
-              <span className="text-lg ">BHIM App</span>
-            </label>
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="phonepe"
+                />
+                <span className="text-lg">PhonePe</span>
+              </label>
+
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="bhim"
+                />
+                <span className="text-lg ">BHIM App</span>
+              </label>
+            </div>
+            <div className="space-y-4">
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="mobikwik"
+                />
+                <span className="text-lg">MobiKwik</span>
+              </label>
+
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="uber"
+                />
+                <span className="text-lg ">Uber Pay</span>
+              </label>
+
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="chillr"
+                />
+                <span className="text-lg">Chillr</span>
+              </label>
+            </div>
+            <div className="space-y-4">
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="paytmpaymentsbank"
+                />
+                <span className="text-lg ">Paytm Payments Bank</span>
+              </label>
+
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="sbipay"
+                />
+                <span className="text-lg ">SBI Pay</span>
+              </label>
+
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="imobile"
+                />
+                <span className="text-lg">iMobile</span>
+              </label>
+            </div>
+            <div className="space-y-4">
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="axispay"
+                />
+                <span className="text-lg ">Axis Pay</span>
+              </label>
+
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="bobupi"
+                />
+                <span className="text-lg">Bank of Baroda (BOB) UPI</span>
+              </label>
+
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="amazonpay"
+                />
+                <span className="text-lg ">Amazon Pay</span>
+              </label>
+            </div>
+            <div className="space-y-4">
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="freecharge"
+                />
+                <span className="text-lg">Freecharge</span>
+              </label>
+
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="cred"
+                />
+                <span className="text-lg ">Cred</span>
+              </label>
+              <label className="flex items-center space-x-2 ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
+                  name="cred"
+                />
+                <span className="text-lg ">Cash on Delivery</span>
+              </label>
+            </div>
           </div>
-          <div className="space-y-4">
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="mobikwik"
-              />
-              <span className="text-lg">MobiKwik</span>
-            </label>
 
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="uber"
-              />
-              <span className="text-lg ">Uber Pay</span>
-            </label>
-
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="chillr"
-              />
-              <span className="text-lg">Chillr</span>
-            </label>
+          <div className="mt-6 flex items-center justify-center">
+            <button
+              type="submit"
+              onClick={() => navigate('/keywordsform')}
+              className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">
+              Submit
+            </button>
           </div>
-          <div className="space-y-4">
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="paytmpaymentsbank"
-              />
-              <span className="text-lg ">Paytm Payments Bank</span>
-            </label>
-
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="sbipay"
-              />
-              <span className="text-lg ">SBI Pay</span>
-            </label>
-
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="imobile"
-              />
-              <span className="text-lg">iMobile</span>
-            </label>
-          </div>
-          <div className="space-y-4">
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="axispay"
-              />
-              <span className="text-lg ">Axis Pay</span>
-            </label>
-
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="bobupi"
-              />
-              <span className="text-lg">Bank of Baroda (BOB) UPI</span>
-            </label>
-
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="amazonpay"
-              />
-              <span className="text-lg ">Amazon Pay</span>
-            </label>
-          </div>
-          <div className="space-y-4">
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="freecharge"
-              />
-              <span className="text-lg">Freecharge</span>
-            </label>
-
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="cred"
-              />
-              <span className="text-lg ">Cred</span>
-            </label>
-            <label className="flex items-center space-x-2 ">
-              <input
-                type="checkbox"
-                className="form-checkbox h-5 w-5 rounded-md border text-blue-500"
-                name="cred"
-              />
-              <span className="text-lg ">Cash on Delivery</span>
-            </label>
-          </div>
-        </div>
-
-        <div className="mt-6 flex items-center justify-center">
-          <button
-            type="submit"
-            onClick={() => navigate('/keywordsform')}
-            className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+      <Footer />
+    </>
   )
 }
 
