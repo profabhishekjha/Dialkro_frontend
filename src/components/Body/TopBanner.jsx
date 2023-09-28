4e587318a2f1a7e421ae77622ea1acf09e05e694
riturajmahapatra <riturajmahapatra@gmail.com>
2023-09-28 17:37:21 +0530

    onlineshopping added

--- src/components/Body/TopBanner.jsx
+++ src/components/Body/TopBanner.jsx
@@ -60,7 +60,9 @@ const TopBanner = () => {
             }}
             className="flex h-full w-full items-center justify-center overflow-hidden rounded-2xl bg-cover bg-center duration-500 hover:scale-105">
             <a
-              href=""
+              onClick={() => {
+                navigate('/onlineshopping')
+              }}
               className=" hidden text-center text-4xl font-extrabold text-white duration-500 hover:scale-[1.5] group-hover:block">
               Order Now!
             </a>
