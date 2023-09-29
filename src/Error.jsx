0d0e6a9ebe1f47e8a8ec00f7f7046379a6bf974d
riturajmahapatra <riturajmahapatra@gmail.com>
2023-09-29 16:28:03 +0530

    error-final

--- src/Error.jsx
+++ src/Error.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 
 import Blob from '/Svgs/Blob.svg'
 import Cart from '/Svgs/Cart.svg'
+import Arrow from '/Svgs/Arrow.svg'
 
 const Error = () => {
   return (
@@ -13,14 +14,17 @@ const Error = () => {
       <div className=" container flex  items-center justify-center max-lg:flex-wrap ">
         <div className="">
           <div className="flex flex-col">
-            <span className="text-[8vw] font-bold max-lg:text-[16vw]">{`Product not available`}</span>
+            <span className="text-[8vw] font-bold text-[#00b4d8] max-lg:text-[16vw]">{`Product not available`}</span>
           </div>
-          <button className=" w-fit rounded-xl border-black bg-slate-300 px-3 py-2 text-[1.5rem]  font-semibold active:bg-purple-400">
-            <Link href={'/'}>Back to Home</Link>
-          </button>
         </div>
         <div className=" relative mr-20 aspect-square h-[25vw] max-lg:h-[100vw]">
-          <img src={Cart} alt="Not Found Robot" style={{ objectFit: 'contain' }} />
+          <Link href={'/'}>
+            <img src={Cart} alt="Not Found Robot" style={{ objectFit: 'contain' }} />
+            <button className=" object-fit flex items-center justify-center rounded-md bg-slate-200 p-5 uppercase hover:underline">
+              <h1 className="text-xl font-semibold ">Back to home</h1>
+              <img src={Arrow} alt="arrow" className="re h-8" />
+            </button>
+          </Link>
         </div>
       </div>
     </main>
