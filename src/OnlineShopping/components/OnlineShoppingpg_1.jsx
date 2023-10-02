91e788a330cad01a389daf568500836bf63395e6
profabhishekjha <jhaabhishek910@gmail.com>
2023-10-02 14:48:15 +0530

    dropdown structure

--- src/OnlineShopping/components/OnlineShoppingpg_1.jsx
+++ src/OnlineShopping/components/OnlineShoppingpg_1.jsx
@@ -7,26 +7,28 @@ const OnlineShoppingpg_1 = () => {
   return (
     <>
       <Nav />
-      <DropdownMenu
-        prompt={'Staples'}
-        image={'http://via.placeholder.com/640x360'}
-        options={['Da', 'hero2']}
-      />
-      <DropdownMenu
-        prompt={'heasro'}
-        image={'http://via.placeholder.com/640x360'}
-        options={['hero1', 'hero2']}
-      />
-      <DropdownMenu
-        prompt={'heasro'}
-        image={'http://via.placeholder.com/640x360'}
-        options={['hero1', 'hero2']}
-      />
-      <DropdownMenu
-        prompt={'heasro'}
-        image={'http://via.placeholder.com/640x360'}
-        options={['hero1', 'hero2']}
-      />
+      <div className="flex items-center justify-center gap-10">
+        <DropdownMenu
+          prompt={'Staples'}
+          image={'http://via.placeholder.com/640x360'}
+          options={['Da', 'hero2']}
+        />
+        <DropdownMenu
+          prompt={'heasro'}
+          image={'http://via.placeholder.com/640x360'}
+          options={['hero1', 'hero2']}
+        />
+        <DropdownMenu
+          prompt={'heasro'}
+          image={'http://via.placeholder.com/640x360'}
+          options={['hero1', 'hero2']}
+        />
+        <DropdownMenu
+          prompt={'heasro'}
+          image={'http://via.placeholder.com/640x360'}
+          options={['hero1', 'hero2']}
+        />
+      </div>
       <Footer />
     </>
   )
