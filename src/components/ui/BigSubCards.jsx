e9c46aecd9ffd3a585dca82381d1adc796bca88a
riturajmahapatra <riturajmahapatra@gmail.com>
2023-10-03 16:53:48 +0530

    changes

--- src/components/ui/BigSubCards.jsx
+++ src/components/ui/BigSubCards.jsx
@@ -4,12 +4,12 @@ const BigSubCards = ({ images, title, onClick }) => {
   return (
     <div className=" container p-3">
       <div className="rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">
-        <a href={onClick} className="cursor-pointer">
+        <a onClick={onClick} className="cursor-pointer">
           <img className="h-24 w-full rounded-t-lg object-cover" src={images} alt="" />
         </a>
         <div className="truncate p-2 text-center">
           <a
-            href={onClick}
+            onClick={onClick}
             className="text-md mb-2 overflow-hidden text-center font-bold capitalize tracking-wide text-gray-900 dark:text-white">
             {title}
           </a>
