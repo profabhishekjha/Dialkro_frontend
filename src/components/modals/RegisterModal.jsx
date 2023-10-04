68278016a732583325afbcc045f846ee35a5b08c
sushil <romanreins488@gmail.com>
2023-10-04 17:30:57 +0530

    Integrated register api

--- src/components/modals/RegisterModal.jsx
+++ src/components/modals/RegisterModal.jsx
@@ -1,11 +1,38 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useState } from 'react'
 
 function RegisterModal({ onClose, onLoginClick }) {
   const handleLoginClick = () => {
     onClose() // Close the LoginModal
     onLoginClick() // Open the RegisterModal
   }
+
+  const [email, setemail] = useState();
+  const [userName, setuserName] = useState();
+  const [password, setpassword] = useState();
+  const [reEnterPassword, setreEnterPassword] = useState();
+
+  const obj = {userName, email, password, reEnterPassword}
+  
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+   let postuserInfo = await fetch('http://localhost:4000/register/user',{
+    method:'post',
+    body:JSON.stringify(obj),
+    headers:{
+      'Content-Type': 'application/json'
+    }
+   });
+   postuserInfo = await postuserInfo.json();
+   localStorage.setItem('user', JSON.stringify(postuserInfo.newUser))
+   localStorage.setItem('token', JSON.stringify(postuserInfo.Token))
+   if(postuserInfo.Token){
+    alert('register successfully !')
+   }else{
+    alert(postuserInfo.msg);
+   }
+  }
+  
   return (
     <div
       id="authentication-modal"
@@ -27,7 +54,7 @@ function RegisterModal({ onClose, onLoginClick }) {
             <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
               Sign up to Dialkro
             </h3>
-            <form className="space-y-6" action="#">
+            <form onSubmit={handleSubmit} className="space-y-6" action="#">
               {/* Form inputs and submit button */}
               <div>
                 <label
@@ -36,6 +63,7 @@ function RegisterModal({ onClose, onLoginClick }) {
                   Your email
                 </label>
                 <input
+                onChange={(e)=>setemail(e.target.value)}
                   type="email"
                   name="email"
                   id="email"
@@ -51,6 +79,7 @@ function RegisterModal({ onClose, onLoginClick }) {
                   Name
                 </label>
                 <input
+                onChange={(e)=>setuserName(e.target.value)}
                   type="text"
                   name="name"
                   id="name"
@@ -66,6 +95,7 @@ function RegisterModal({ onClose, onLoginClick }) {
                   Your password
                 </label>
                 <input
+                onChange={(e)=>setpassword(e.target.value)}
                   type="password"
                   name="password"
                   id="password"
@@ -81,6 +111,7 @@ function RegisterModal({ onClose, onLoginClick }) {
                   Re-enter your password
                 </label>
                 <input
+                onChange={(e)=>setreEnterPassword(e.target.value)}
                   type="password"
                   name="password"
                   id="password"
